cc98b44766b4f64791c2cbfa43340316443d13f8
Novozhilov, Sergei <sergei.novozhilov@kronos.com>
2018-12-02 16:54:45 +0300

    commit

--- src/main/java/ru/otus/ConsoleService/ConsoleServiceImpl.java
+++ src/main/java/ru/otus/ConsoleService/ConsoleServiceImpl.java
@@ -1,23 +1,54 @@
 package ru.otus.ConsoleService;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import lombok.Setter;
+import ru.otus.Scanner.IScanner;
+import ru.otus.Scanner.ScannerImpl;
+
+import java.util.*;
 
 public class ConsoleServiceImpl implements IConsoleService {
-	@Override public void setQuestions(Map<String, List<String>> questions) {
 
+	private IScanner scanner = new ScannerImpl();
+
+	@Setter
+	private Map<String, List<String>> questions;
+
+	@Override
+	public Map<String, String> getAnswers() {
+		Map<String, String> answers = new HashMap<>();
+		System.out.println("Please answer the questions: ");
+		questions.forEach((k, v) -> {
+			Collections.shuffle(v);
+			boolean gotAnswer = false;
+			while (!gotAnswer) {
+				gotAnswer = true;
+				for (int i = 0; i < v.size(); i++) {
+					System.out.println((i + 1) + ") " + v.get(i));
+				}
+				String answer = scanner.getInput();
+				int number = -1;
+				try {
+					number = Integer.getInteger(answer);
+					if (number <= 0 || number >= v.size()) {
+						gotAnswer = false;
+					}
+				} catch (Exception e) {
+					gotAnswer = false;
+				}
+				if (gotAnswer) {
+					answers.put(k, v.get(number - 1));
+				}
+			}
+		});
+
+		return answers;
 	}
 
-	@Override public Map<String, String> getAnswers() {
-		return null;
-	}
-
-	@Override public String getName() {
-		String username = "";
+	@Override
+	public String getName() {
+		String username;
 		System.out.println("Enter your full name: ");
-		Scanner scanner = new Scanner(System.in);
-		username = scanner.nextLine();
+		username = scanner.getInput();
 
 		return username;
 	}
--- src/main/java/ru/otus/ConsoleService/IConsoleService.java
+++ src/main/java/ru/otus/ConsoleService/IConsoleService.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Map;
 
 public interface IConsoleService {
-	void setQuestions(Map<String, List<String>> questions);
 	Map<String, String> getAnswers();
 	String getName();
 }
