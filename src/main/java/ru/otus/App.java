1e92dbef4893bf4d15306c3bbf529396e3d5dfb2
SergeyNovozhilov <sergey.n.novozhilov@gmail.com>
2018-12-28 22:26:10 +0400

    first for 4

--- src/main/java/ru/otus/App.java
+++ src/main/java/ru/otus/App.java
@@ -2,11 +2,9 @@ package ru.otus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import ru.otus.Config.ApplicationProps;
 
 @SpringBootApplication
-@EnableConfigurationProperties(ApplicationProps.class)
+//@EnableConfigurationProperties(ApplicationProps.class)
 public class App {
 
 	public static void main(String[] args) {
