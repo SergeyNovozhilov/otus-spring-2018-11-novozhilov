965d348a4f03fdffbffa603f37801ed4edf383c5
SergeyNovozhilov <sergey.n.novozhilov@gmail.com>
2019-02-10 12:30:28 +0400

    commit

--- src/main/java/ru/otus/Repositories/BookRepository.java
+++ src/main/java/ru/otus/Repositories/BookRepository.java
@@ -15,7 +15,7 @@ public interface BookRepository extends JpaRepository<Book, UUID> {
 	@Query("select distinct b from Book b left join fetch b.comments c")
 	List<Book> findAll();
 
-	@Query("select distinct b from Book b left join fetch b.comments c where b.title = : title")
+	@Query("select distinct b from Book b left join fetch b.comments c where b.title = :title")
 	Collection<Book> findByTitle(@Param("title") String title);
 
 	@Query("select distinct b FROM Book b JOIN b.authors a left join fetch b.comments c where a.name = :author")
